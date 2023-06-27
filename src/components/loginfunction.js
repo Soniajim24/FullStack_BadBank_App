272338ffe0538a03f527418dbfb912f13c1e54bb
Sonia Jimenez <112973252+Soniajim24@users.noreply.github.com>
2023-06-27 16:09:02 +0200

    Delete src directory

--- src/components/loginfunction.js
+++
@@ -1,8 +0,0 @@
-export const login = async () => {
-    return {
-        name: "Bobby",
-        email: "bobby@mit.edu",
-        password: "secret11",
-        balance: 0,
-    };
-};
--- src/pages/_app.js
+++
@@ -1,8 +0,0 @@
-/* Global styles fixed with this strategy */
-import './styles/globals.module.css'
-
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <Component {...pageProps} />
-  );
-}
