bddaea97e5b6e502a547f8c501cfc52374e9d1ba
Sonia Jimenez <112973252+Soniajim24@users.noreply.github.com>
2023-07-30 19:33:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,29 @@ app.use(express.json()); // Add this line to parse JSON data from requests
 // Use the API routes and middleware
 app.use("/api", apiRoutes);
 
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var express = require('express')
+var timeout = require('connect-timeout')
+
+// example of using this top-level; note the use of haltOnTimedout
+// after every middleware; it will stop the request flow on a timeout
+
+app.use(timeout('5s'))
+app.use(bodyParser())
+app.use(haltOnTimedout)
+app.use(cookieParser())
+app.use(haltOnTimedout)
+
+// Add your routes here, etc.
+
+function haltOnTimedout (req, res, next) {
+  if (!req.timedout) next()
+}
+
+app.listen(3000)
+
+
 const port = 3000;
 app.listen(port, () => {
   console.log("Running on port: " + port);
